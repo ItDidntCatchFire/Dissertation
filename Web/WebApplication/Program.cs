e748c3a43ea4c8e50b3aa836eec052997f009a1d
ItDidntCatchFire <deaddanster@googlemail.com>
2020-04-10 05:22:16 +0100

    Added web app and changed default address

---
+++ Web/WebApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+
+            return host;
+        }
+    }
+}
