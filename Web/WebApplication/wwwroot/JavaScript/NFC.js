fa95efcd840643df2f006ed8e1e9dd6b30f1fe28
ItDidntCatchFire <deaddanster@googlemail.com>
2020-04-10 08:40:29 +0100

    We now have a call back

--- Web/WebApplication/wwwroot/JavaScript/NFC.js
+++ Web/WebApplication/wwwroot/JavaScript/NFC.js
@@ -1,24 +1,21 @@
+// w3chttps://w3c.github.io/web-nfc/
+
 window.NFCScan = async () => {
-    alert("User clicked scan button");
-    console.log("User clicked scan button");
     try {
         const reader = new NDEFReader();
         await reader.scan();
-        console.log("> Scan started");
-        alert("> Scan started");
         
         reader.addEventListener("error", () => {
-            alert(`Argh! ${error.message}`);
-            log(`Argh! ${error.message}`);
+            DotNet.invokeMethod('BlazorSample', 'UpdateMessageCaller', `Error! ${error.message}`);
         });
 
         reader.addEventListener("reading", ({ message, serialNumber }) => {
-            alert(`> Serial Number: ${serialNumber}`);
-            console.log(`> Serial Number: ${serialNumber}`);
-            console.log(`> Records: (${message.records.length})`);
+            //console.log(`> Serial Number: ${serialNumber}`);
+            //console.log(`> Records: (${message.records.length})`);
+
+            DotNet.invokeMethod('WebApplication', 'UpdateMessageCaller', "Serial Number: " + serialNumber);
         });
     } catch (error) {
-        console.log("Argh! " + error);
-        alert("Argh! " + error);
+        DotNet.invokeMethod('WebApplication', 'UpdateMessageCaller', "Error " + error);
     }
-}
+};
