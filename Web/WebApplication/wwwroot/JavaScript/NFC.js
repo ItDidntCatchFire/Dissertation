6629ca05a5cf2e453e178ed6a115192e21819f8f
ItDidntCatchFire <deaddanster@googlemail.com>
2020-04-13 05:04:50 +0100

    Mobile now incrementing count and showing total price, base functionaility done

--- Web/WebApplication/wwwroot/JavaScript/NFC.js
+++ Web/WebApplication/wwwroot/JavaScript/NFC.js
@@ -1,28 +1,20 @@
 // w3chttps://w3c.github.io/web-nfc/
 
-window.NFCPermission = () => {
-        const reader = new NDEFReader();
-        reader.scan();
-};
-
 window.NFCScan = async () => {
     try {
         const reader = new NDEFReader();
         await reader.scan();
         
-        reader.addEventListener("error", () => {
-            DotNet.invokeMethod('WebApplication', 'UpdateMessageCaller', `Error! ${error.message}`);
-        });
-
         reader.addEventListener("reading", ({ message, serialNumber }) => {
             var record = message.records[0];
             const textDecoder = new TextDecoder(record.encoding);
             var data = textDecoder.decode(record.data);
-            DotNet.invokeMethod('WebApplication', 'UpdateMessageCaller', "Message: " + data);
+            
+            DotNet.invokeMethod('WebApplication', 'UpdateMessageCaller', data);
         });
+        
         return true;
     } catch (error) {
-        DotNet.invokeMethod('WebApplication', 'UpdateMessageCaller', "Error " + error);
         return false;
     }
 };
